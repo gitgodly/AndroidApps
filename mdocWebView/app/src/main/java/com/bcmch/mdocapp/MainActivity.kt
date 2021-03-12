0528081af164d836d4b681930d676446b50c5992
git.godly <git.godly@gmail.com>
2021-03-12 22:45:57 +0530

    Added Javascript Supports

--- mdocWebView/app/src/main/java/com/bcmch/mdocapp/MainActivity.kt
+++ mdocWebView/app/src/main/java/com/bcmch/mdocapp/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.bcmch.mdocapp
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val webViewClient = WebViewClient()
         val webView = WebView(this)
-
         webView.webViewClient=WebViewClient()
         webView.webChromeClient = WebChromeClient();
         webView.settings.setJavaScriptEnabled(true);
+        webView.settings.setDomStorageEnabled(true);
+        webView.addJavascriptInterface(WebAppInterface(this), "Android")
         setContentView(webView)
-        webView.loadUrl("https://mdoc.mybcmch.com/test.html")
+        webView.loadUrl("https://tm.mybcmch.com");
+
+
+    }
+
+
+}
+/** Instantiate the interface and set the context  */
+class WebAppInterface(private val mContext: Context) {
+
+    /** Show a toast from the web page  */
+    @JavascriptInterface
+    fun showToast(toast: String) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_LONG).show()
     }
 }
